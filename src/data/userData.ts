7c03219e9a1d2b941f90de825d6aa54468f4dc53
thiago <thiagorerezende@gmail.com>
2025-08-28 14:46:33 -0300

    left and right part done going to middle , todo: create header home

---
+++ src/data/userData.ts
@@ -0,0 +1,11 @@
+import { User } from "@/types/user";
+
+export const user: User = {
+    slug: 'thiago',
+    name: 'thiago rezende',
+    avatar: 'https://p2.trrsf.com/image/fget/cf/360/600/images.terra.com/2024/02/20/859991889-avatar-a-lenda-de-aang.jpg',
+    cover: 'https://p2.trrsf.com/image/fget/cf/360/600/images.terra.com/2024/02/20/859991889-avatar-a-lenda-de-aang.jpg',
+    bio: 'dev',
+    link: 'https://app.b7web.com.br/lesson/40a4481d-b770-4cfd-bc79-e5a52da979d7',
+    postCount: 128
+}
