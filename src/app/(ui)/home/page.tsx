f8801a217f96a0f52f2f9e7a484e7487b0864ea4
thiago <thiagorerezende@gmail.com>
2025-08-31 14:37:33 -0300

    add mobile first header with tweet posts, interactive menu for mobile, good work

--- src/app/(ui)/home/page.tsx
+++ src/app/(ui)/home/page.tsx
@@ -1,5 +1,11 @@
+import { HomeHeader } from "@/components/home/home-headet";
+import { TweetPost } from "@/components/tweet/tweet-post";
+
 export default function Page(){
     return(
-        <div>..</div>
+        <div>
+            <HomeHeader/>
+            <TweetPost/>
+        </div>
     )
 }
